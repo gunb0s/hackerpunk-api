076e35c3f2b7236ee8e5f6d51ec61cafb2947ac5
gunb0s <bys20411@gmail.com>
2022-04-19 20:21:48 +0900

    add createwallet

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,32 @@
-import * as pluralize from "pluralize";
+import * as lightwallet from "eth-lightwallet";
 
-export function getPlural(str: any): string {
-  return pluralize.plural(str);
+/**
+ * @method: returns address and privateKey
+ * @param pwd string
+ * @return: {string, string}
+ */
+export function createWallet(pwd: string): any {
+  return new Promise((resolve, reject) => {
+    let secretSeed: string = lightwallet.keystore.generateRandomSeed();
+
+    lightwallet.keystore.createVault(
+      {
+        password: pwd,
+        seedPhrase: secretSeed,
+        hdPathString: "m/0'/0'/0'",
+      },
+      (err, ks) => {
+        if (err) reject(err);
+        ks.keyFromPassword(pwd, (err, pwDeriveKey) => {
+          if (err) reject(err);
+          ks.generateNewAddress(pwDeriveKey, 1);
+
+          let address = ks.getAddresses().toString();
+          let privateKey = ks.exportPrivateKey(address, pwDeriveKey);
+
+          resolve({ address, privateKey });
+        });
+      }
+    );
+  });
 }
